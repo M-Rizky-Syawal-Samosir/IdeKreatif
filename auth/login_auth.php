7068f272e6eb438ee511e2572a3bb01e1a5ef321
Skywalkyy <herogemer749@gmail.com>
2025-01-20 11:38:49 +0700

    preetier

--- auth/login_auth.php
+++ auth/login_auth.php
@@ -1,14 +1,18 @@
 <?php
 session_start();
 require_once("../config.php");
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
+
     $sql = "SELECT * FROM users WHERE username = '$username'";
     $result = $conn->query($sql);
+
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         // Verifikasi Password
+
         if (password_verify($password, $row["password"])) {
             $_SESSION["username"] = $username;
             $_SESSION["name"] = $row["name"];
--- auth/logout.php
+++ auth/logout.php
@@ -3,4 +3,4 @@ session_start(); // Memulai sesi
 session_start(); // Menghapus semua data sesi
 session_destroy(); //Menghancurkan sesi sepenuhnya
 header('location: login.php'); // Arahkan pengguna ke halaman login
-exit(); // Menghentikan eksekusi script
+exit(); // Menghentikan eksekusi scriptdsp
